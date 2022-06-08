436ee30b136e04c6708a00ce528b68e25a71e001
Andrey <64960247+Panfil-spb@users.noreply.github.com>
2022-06-08 17:12:06 +0300

    Update insertion sort.py

--- insertion sort.py
+++ insertion sort.py
@@ -1,5 +1,3 @@
-import random
-
 def insertion_sort(mas: list):
     for i in range(1, len(mas)):
         j = i - 1
@@ -7,11 +5,3 @@ def insertion_sort(mas: list):
         while j >= 0 and mas[j] > value:
             j -= 1
         mas.insert(j + 1, value)
-
-
-mas = []
-for i in range(random.randrange(1, 100)):
-    mas.append(random.randrange(1, 100))
-print(mas)
-insertion_sort(mas)
-print(mas)
